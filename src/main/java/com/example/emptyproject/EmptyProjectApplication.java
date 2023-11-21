c1775709ae5c88dadd48ac5442dab74b3f232198
jungmin.park <pjm9673@gmail.com>
2023-11-21 10:02:29 +0900

    init commit

---
+++ src/main/java/com/example/emptyproject/EmptyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.emptyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyProjectApplication.class, args);
+	}
+
+}
