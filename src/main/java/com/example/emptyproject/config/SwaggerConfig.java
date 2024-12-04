b9b1fc9e8bc791cc174db969ba6e6bdf00bb0996
jungmin.park <pjm9673@gmail.com>
2024-12-05 00:42:10 +0900

    .

--- src/main/java/com/example/emptyproject/config/SwaggerConfig.java
+++ src/main/java/com/example/emptyproject/config/SwaggerConfig.java
@@ -1,2 +1,25 @@
-package com.example.emptyproject.config;public class SwaggerConfig {
+package com.example.emptyproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.OAS_30)
+//            .apiInfo(apiInfo())
+//            .select()
+//            .apis(RequestHandlerSelectors.any())
+//            .paths(PathSelectors.any())
+//            .build();
+//    }
+//
+//    private ApiInfo apiInfo(){
+//        return new ApiInfoBuilder()
+//            .title("test")
+//            .description("description")
+//            .version("1.0.0")
+//            .build();
+//    }
 }
--- src/main/java/com/example/emptyproject/controller/TestController.java
+++ src/main/java/com/example/emptyproject/controller/TestController.java
@@ -1,2 +1,15 @@
-package com.example.emptyproject.controller;public class TestController {
+package com.example.emptyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/swagger")
+public class TestController {
+    @GetMapping
+    public String test(){
+        return "hi";
+    }
 }
